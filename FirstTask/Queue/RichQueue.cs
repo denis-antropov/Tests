205c48c8ad7321ce39dcd9d225601a82fe78bfdb
Денис <denis.antropov@outlook.com>
2017-09-23 18:55:01 +0500

    First task completed

---
+++ FirstTask/Queue/RichQueue.cs
@@ -0,0 +1,130 @@
+﻿namespace Queue
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    /// <summary>
+    /// Represents a queue with rich functionality
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class RichQueue<T> : IDisposable
+    {
+        /// <summary>
+        /// Inner queue
+        /// </summary>
+        private readonly Queue<T> _queue;
+
+        /// <summary>
+        /// Allows only one thread work inside of Pop method at one time
+        /// </summary>
+        private readonly object _popSync;
+
+        /// <summary>
+        /// Reset event for waiting new item when Pop method is called
+        /// </summary>
+        private readonly ManualResetEvent _resetEvent;
+
+        /// <summary>
+        /// Flag indicating that instance is disposed or not
+        /// </summary>
+        private bool _disposed;
+
+        /// <summary>
+        /// Initializes a new instance of the RichQueue class
+        /// </summary>
+        public RichQueue()
+        {
+            _queue = new Queue<T>();
+            _resetEvent = new ManualResetEvent(false);
+            _popSync = new object();
+            _disposed = false;
+        }
+
+        /// <summary>
+        /// Gets the number of elements contained in queue
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                if (_disposed)
+                    throw new ObjectDisposedException("Current instance is disposed");
+
+                lock (_queue)
+                {
+                    return _queue.Count;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds an object to the quue
+        /// </summary>
+        /// <param name="item">New item</param>
+        public void Push(T item)
+        {
+            if (_disposed)
+                throw new ObjectDisposedException("Current instance is disposed");
+
+            lock (_queue)
+            {
+                _queue.Enqueue(item);
+                _resetEvent.Set();
+            }
+        }
+
+        /// <summary>
+        /// Removes and returns the object at the beginning of the queue.
+        /// If no queued items, then waits new one
+        /// </summary>
+        /// <returns>First item in the queue</returns>
+        public T Pop()
+        {
+            if(_disposed)
+                throw new ObjectDisposedException("Current instance is disposed");
+
+            lock (_popSync)
+            {
+                bool needWait = false;
+                lock (_queue)
+                {
+                    if (_queue.Count == 0)
+                    {
+                        // We should wait while queue will not be empty
+                        needWait = true;
+                        _resetEvent.Reset();
+                    }
+                }
+
+                if (needWait)
+                {
+                    // Wait signal from Push method
+                    _resetEvent.WaitOne();
+                }
+
+                lock (_queue)
+                {
+                    // Now, we sure that queue has items
+                    return _queue.Dequeue();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting
+        /// unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            if (_disposed)
+                return;
+
+            _resetEvent.Dispose();
+            _queue.Clear();
+            _disposed = true;
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ FirstTask/UnitTests/RichQueueTests.cs
@@ -0,0 +1,160 @@
+﻿namespace UnitTests
+{
+    using NUnit.Framework;
+    using Queue;
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class RichQueueTests
+    {
+        [Test]
+        public void PushIncreasesCount()
+        {
+            using (RichQueue<int> richQueue = new RichQueue<int>())
+            {
+                richQueue.Push(5);
+                richQueue.Push(7);
+
+                Assert.AreEqual(2, richQueue.Count);
+            }
+        }
+
+        [Test]
+        public void PopReturnsPushedItem()
+        {
+            using (RichQueue<int> richQueue = new RichQueue<int>())
+            {
+                richQueue.Push(5);
+                var actualValue = richQueue.Pop();
+
+                Assert.AreEqual(5, actualValue);
+            }
+        }
+
+        [Test]
+        public void PopDecreasesCount()
+        {
+            using (RichQueue<int> richQueue = new RichQueue<int>())
+            {
+                richQueue.Push(5);
+                var countBeforePop = richQueue.Count;
+
+                richQueue.Pop();
+
+                Assert.AreEqual(countBeforePop - 1, richQueue.Count);
+            }
+        }
+
+        [Test]
+        public void PopTwiseReturnsTwoPushedItem()
+        {
+            using (RichQueue<int> richQueue = new RichQueue<int>())
+            {
+                richQueue.Push(5);
+                richQueue.Push(7);
+
+                var actualValue1 = richQueue.Pop();
+                var actualValue2 = richQueue.Pop();
+
+                Assert.AreEqual(5, actualValue1);
+                Assert.AreEqual(7, actualValue2);
+            }
+        }
+
+        [Test]
+        public void PopWaitsWhenItemWillBeAvailable()
+        {
+            using (RichQueue<int> richQueue = new RichQueue<int>())
+            {
+                ManualResetEvent resetEvent = new ManualResetEvent(false);
+                var task = Task.Run(() =>
+                {
+                    resetEvent.Set();
+
+                    return richQueue.Pop();
+                });
+
+                // Wait when Pop task begin
+                resetEvent.WaitOne();
+                richQueue.Push(5);
+
+                Assert.AreEqual(5, task.Result);
+            }
+        }
+
+        [Test]
+        public void TwoPopsWaitWhenItemsWillBeAvailable()
+        {
+            using (RichQueue<int> richQueue = new RichQueue<int>())
+            {
+                AutoResetEvent resetEvent = new AutoResetEvent(false);
+
+                var task1 = Task.Run(() =>
+                {
+                    resetEvent.Set();
+                    return richQueue.Pop();
+                });
+
+                // Wait when Pop task begin
+                resetEvent.WaitOne();
+
+                var task2 = Task.Run(() =>
+                {
+                    resetEvent.Set();
+                    return richQueue.Pop();
+                });
+
+                // Wait when Pop task begin
+                resetEvent.WaitOne();
+
+                richQueue.Push(5);
+                richQueue.Push(7);
+
+                Assert.AreEqual(5, task1.Result);
+                Assert.AreEqual(7, task2.Result);
+            }
+        }
+
+        [Test]
+        public void PushThrowsOnDisposedObject()
+        {
+            RichQueue<int> richQueue = new RichQueue<int>();
+            richQueue.Dispose();
+
+            Assert.Catch<ObjectDisposedException>(() => richQueue.Push(5));
+        }
+
+        [Test]
+        public void PopThrowsOnDisposedObject()
+        {
+            RichQueue<int> richQueue = new RichQueue<int>();
+            richQueue.Push(5);
+            richQueue.Dispose();
+
+            Assert.Catch<ObjectDisposedException>(() => richQueue.Pop());
+        }
+
+        [Test]
+        public void CountThrowsOnDisposedObject()
+        {
+            RichQueue<int> richQueue = new RichQueue<int>();
+            richQueue.Push(5);
+            richQueue.Dispose();
+
+            Assert.Catch<ObjectDisposedException>(() =>
+            {
+                var count = richQueue.Count;
+            });
+        }
+
+        [Test]
+        public void SeveralDisposeNotThrowsExceptions()
+        {
+            RichQueue<int> richQueue = new RichQueue<int>();
+            richQueue.Dispose();
+            richQueue.Dispose();
+        }
+    }
+}
