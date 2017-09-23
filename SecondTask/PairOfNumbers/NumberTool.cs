10dd001a426b7f5515a6369adf8d094d15fd4ff3
Денис <denis.antropov@outlook.com>
2017-09-23 12:02:21 +0500

    First test of NumberTool was created

---
+++ SecondTask/PairOfNumbers/NumberTool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PairOfNumbers
+{
+    public class NumberTool
+    {
+        public List<int> MatchSumOfPairs(IEnumerable<int> inputCollection)
+        {
+            return new List<int>();
+        }
+    }
+}
---
+++ SecondTask/UnitTests/NumberToolTests.cs
@@ -0,0 +1,21 @@
+﻿namespace UnitTests
+{
+    using NUnit.Framework;
+    using PairOfNumbers;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class NumberToolTests
+    {
+        [Test]
+        public void ReturnsEmptyCollectionOfPairsIfNoMatchSum()
+        {
+            var inputCollection = new List<int> { 1, 7, 8, 15 };
+            NumberTool pairOfNumbers = new NumberTool();
+
+            var outputCollection = pairOfNumbers.MatchSumOfPairs(inputCollection);
+
+            CollectionAssert.IsEmpty(outputCollection);
+        }
+    }
+}
